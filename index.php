cd4592d8275aac5ea880880f85651c26c3d5bb02
jatinder <jssingh134@gmail.com>
2025-04-30 09:32:37 +1000

    update the colours and design of the website

---
+++ index.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jatidner singh | Web developer</title>
+
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+        integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="stylesheet"
+        href="https://owlcarousel2.github.io/OwlCarousel2/assets/owlcarousel/assets/owl.carousel.min.css">
+    <link rel="stylesheet"
+        href="https://owlcarousel2.github.io/OwlCarousel2/assets/owlcarousel/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</head>
+
+<body>
+    <div class="loader">
+        <img src="img/loader.gif" class="img-fluid" alt="">
+        <h4>loading....</h4>
+    </div>
+    <section class="head">
+        <div class="container">
+            <header data-aos="fade-up">
+                <nav class="navbar navbar-expand-lg navbar-dark custmnav">
+                    <a class="navbar-brand" href="#">
+                        
+                       | JatinderDev |
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto ">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#services">services</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#skill">skill</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#portfolio">portfolio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact">contact</a>
+                            </li>
+                        </ul>
+                        <div class="form-inline my-2 my-lg-0">
+                        
+                            <a href="" class="custmbtn_small">Contact us</a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="bannertxt" data-aos="fade-right">
+                        <h1>I am Jatinder Singh, <br>
+                     <span>   Web Developer & Designer </span><br>
+                        Based in Melbourne.
+                        </h1>
+                        <p>I have been working as a <b>full stack developer</b> for the past 4 years.
+                            I build responsive websites and web applications in multiple languages as required to the
+                            project and use latest
+                            technonlogy to boost the speed of a site.
+                        </p>
+                        <a target="_blank" href="https://api.whatsapp.com/send?phone=+919041737383&text=hi"
+                            class="custmbtn">View My Portfolio &nbsp;<i class="fa fa-arrow-down" aria-hidden="true"></i></a>
+                    </div>
+
+                </div>
+                <div class="col-md-5">
+                    <div class="bannerimg" data-aos="fade-left">
+                        <img src="img/banner.png"  class="img-fluid" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <img src="img/servicebar.png" class="img-fluid" width="100%" alt="">
+    <section class="services" id="services">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <div class="servicesbox">
+                        <div class="servicecard" data-aos="zoom-in">
+                            <div class="cardimg">
+                                <img src="img/deveolper.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="cardtxt">
+                                <h4>website designer</h4>
+                                <p>designe is what make imperssion on your client i can make fully responsive design
+                                    </p>
+                                    <a href="" >Read More</a>
+
+
+                            </div>
+                        </div>
+                        <div class="servicecard" data-aos="zoom-in">
+                            <div class="cardimg">
+                                <img src="img/deveolper.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="cardtxt">
+                                <h4>web developer</h4>
+                                <p>develop your backend with smart programming languages so your client can access
+                                                                  </p>
+                                    <a href="" >Read More</a>
+
+                            </div>
+                        </div>
+
+                        <div class="servicecard" data-aos="zoom-in">
+                            <div class="cardimg">
+                                <img src="img/deveolper.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="cardtxt">
+                                <h4>digital marketing</h4>
+                                <p>pormotion of your website is cruical part of your business i will help you to
+                                  
+                                </p>
+                                <a href="" >Read More</a>
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-md-6">
+                    <div class="bannertxt" data-aos="fade-left">
+                        <h2 class="subHeading">
+                            Service I Provide
+                        </h2>
+                        <p class="text-justify">i am provideing three main services which essentially for every
+                            website.first is design which make impression on your client
+                            and help to discover what he/she wants from your service.The second one is development ,i
+                            will providie fast and secure service to
+                            your client is what most preferable for me.therefore client can give you information without
+                            heistance and feel secure.digital
+                            marketing can genrate organic traffic to your website and help people to understad what you
+                            business actually is .i will make
+                            social campaigns and Ads to boost your business.
+
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="about" id="skill">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6"></div>
+                <div class="col-md-6">
+                    <div class="abouttxt">
+                        <br>
+                        <div class="my-3">
+                           <h2 class="subHeading"><span>About Me</span></h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur nisi possimus repellendus ipsam pariatur nam in expedita error recusandae dolore, perspiciatis corrupti sapiente id, et harum! Deserunt aut molestiae eligendi. Dolores et quibusdam consequatur labore magnam quam? Nihil eveniet, exercitationem quis voluptate voluptatum, unde nostrum tempore repellendus quasi commodi eligendi.</p>
+                        </div>
+                       
+                        <div class="row">
+                            <div class="col-md-6">
+                            <div class="aboutthumb">
+                                <h3>10+</h3>
+                                <p>Project Completed</p>
+                                </div>
+                               
+                            </div>
+                            <div class="col-md-6">
+                            <div class="aboutthumb">
+                                <h3>5+</h3>
+                                <p>Years of Experience</p>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        
+            </div>
+        </div>
+    </section>
+    <section class="projects" id="portfolio">
+    <div class="container">
+        <h2 class="subHeading">My Latest <span>Projects</span></h2>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="project">
+                    <div class="projectImg">
+                        
+                    </div>
+                    <div class="projectTitle">
+                        <h4>Brighton Institue of Technonlogy (website)</h4>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+    </div>
+    </section>
+    
+ 
+    <section class="contact" id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="contacttxt" data-aos="fade-right">
+                        <h1 class="mb-3">contact us
+                        </h1>
+                        <p>have a project? discuss with us get free assistance and get road map of your start up.drop a
+                            mail or call</p>
+                        <ul class="contactul mt-4">
+                            <li> <span><i class="fa fa-phone-square" aria-hidden="true"></i> : </span> <a
+                                    href="">+919041737383</a></li>
+                            <li> <span><i class="fa fa-envelope"></i> : </span> <a href="">jssingh134@gmail.com</a></li>
+
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="contactform" data-aos="fade-left">
+                        <form action="" method="post">
+                            <label for="">name</label>
+                            <input type="text" name="name" id="name" class="txtbox">
+                            <label for="">email*</label>
+                            <input required type="email" name="email" id="email" class="txtbox">
+                            <label for="">write your message*</label>
+                            <textarea required name="msg" id="msg" cols="20" class="txtbox" rows="3"></textarea>
+
+                            <hr>
+                            <button type="submit" class="btn btn-block custmbtn">Submit <i class="fa fa-send"></i>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <p>all rights reserved.&copy; jatinder singh</p>
+        </div>
+
+    </footer>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function () {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/61e3d180b84f7301d32b4045/1fpukljp2';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+</body>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous">
+</script>
+
+<script src="https://owlcarousel2.github.io/OwlCarousel2/assets/owlcarousel/owl.carousel.js"></script>
+<script>
+    $(document).ready(function () {
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            autoplay: true,
+            autoplayTimeout: 1500,
+            autoplayHoverPause: true,
+            dotsEach: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: true
+                },
+                600: {
+                    items: 3,
+                    nav: false
+                },
+                1000: {
+                    items: 4,
+                    nav: false,
+                    loop: true,
+                    margin: 20
+                }
+            }
+        })
+    })
+</script>
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+    AOS.init();
+    $(window).on('load', function () {
+        $(".loader").hide();
+
+
+    });
+</script>
+
+</html>
